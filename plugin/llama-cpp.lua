fb58de31b17da538ffe7d2405d58613ab16fdb39
edoardomussi <mussi.edoardo@hotmail.it>
2026-07-02 22:18:15 +0200

    Added llama-cpp.lua

--- plugin/llama-cpp.lua
+++ plugin/llama-cpp.lua
@@ -5,5 +5,5 @@ vim.g.loaded_llama_cpp_nvim = true
 
 -- Point directly to the streamlined .execute() pipeline
 vim.api.nvim_create_user_command("Llama", function(opts)
-    require("llama-cpp").execute(opts)
+    require("llama-cpp").execute_request(opts)
 end, { nargs = "*", range = true , desc = "Trigger the llama.cpp generation engine" })
